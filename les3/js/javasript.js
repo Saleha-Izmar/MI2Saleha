bff39fae62e2cd77da1c90957a11a8ae605c1136
Saleha-Izmar <43605619+Saleha-Izmar@users.noreply.github.com>
2019-02-28 12:34:41 +0100

    Add files via upload

---
+++ les3/js/javasript.js
@@ -0,0 +1,18 @@
+;(function() {
+	/*global window, document */
+  'use strict';
+
+ 
+  window.addEventListener('load', function() {
+    
+    document.getElementById('btnLogin').addEventListener('click', function(){
+		
+		document.getElementById('loginmodal').classList.remove('loginmodal--verborgen')
+	});
+
+    document.getElementById('btnCancel').addEventListener('click', function(){
+		
+		document.getElementById('loginmodal').classList.add('loginmodal--verborgen')
+	});
+  });
+})();
